849ac0d7092e32e5014a91d8d301c26082e497d3
ZHONGHUIDI <31836722+rachalzzz@users.noreply.github.com>
2019-03-04 09:54:48 +0800

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -2,17 +2,24 @@ window.onload = function () {
 	var whdef = 100 / 1920; // 表示1920的设计图,使用100PX的默认值 
 	var whdef_m = 100 / 750;
 	var wW = window.innerWidth; // 当前窗口的宽度
-	// var rem = wW * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值 
+	var rem = wW * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值 
+	if(rem>=100){
+		rem=90
+	}	
 	if (wW > 750) {
-		document.getElementsByTagName('html')[0].style.fontSize = wW * whdef + 'px';
+		document.getElementsByTagName('html')[0].style.fontSize = rem + 'px';
 	} else {
 		document.getElementsByTagName('html')[0].style.fontSize = whdef_m * wW + 'px';
 	}
 	window.onresize = function () {
 		var wW = window.innerWidth; // 当前窗口的宽度
+		var rem = wW * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值 
+		if(rem>=100){
+			rem=90
+		}
 		console.log(wW)
 		if (wW > 750) {
-			document.getElementsByTagName('html')[0].style.fontSize = wW * whdef + 'px';
+			document.getElementsByTagName('html')[0].style.fontSize = rem + 'px';
 		} else {
 			document.getElementsByTagName('html')[0].style.fontSize = whdef_m * wW + 'px';
 		}
